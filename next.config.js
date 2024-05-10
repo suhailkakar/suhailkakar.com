a534a5f60c9021a11df265e8a7ee9db86abc9879
Suhail Kakar <suhail.zahed@gmail.com>
2024-05-10 06:06:43 +0530

    fix: add domain to next.config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'github.com',
+                hostname: 'suhailkakar-com.vercel.app',
             },
         ],
     }
