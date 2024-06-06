859d0ddf7d13ac70c0a70c52fcb730ea714ddbe2
Suhail Kakar <suhail.zahed@gmail.com>
2024-06-06 21:08:01 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'suhailkakar-com.vercel.app',
             },
         ],
+    }, 
+    redirects: async () => {
+        return [
+            {
+                source: '/the-complete-developers-guide-to-eigenlayers-avs',
+                destination: '/the-complete-developers-guide-to-avs',
+                permanent: true,
+            },
+        ];
     }
+
 };
 
 module.exports = nextConfig;
