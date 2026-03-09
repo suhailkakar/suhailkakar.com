65b802798a82d85ab0f5029e9d7611ebb4f0f127
Suhail Kakar <suhail.zahed@gmail.com>
2026-03-09 08:40:32 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Oooh_Baby } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const ooohBaby = Oooh_Baby({
@@ -74,6 +75,7 @@ export default function RootLayout({
         className={` ${ooohBaby.variable} font-sans antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
